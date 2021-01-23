08bc8e4a3e87bbd052ce0097f42ef7d9c11fc09e
tangyun <tangyun1992@qq.com>
2021-01-23 23:14:56 +0800

    完成columnList组件模拟数据渲染

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1专栏',
+    description: '这是test1专栏，有一段有意思的简介，可以更新一下',
+    avatar: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
+  },
+  {
+    id: 2,
+    title: 'test2专栏',
+    description: '这是test2专栏，有一段有意思的简介，可以更新一下',
+    avatar: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
+  }
+]
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld
+  name: 'App',
+  components: { ColumnList },
+  setup() {
+    return {
+      list: testData
+    }
   }
-});
+})
 </script>
 
 <style>
