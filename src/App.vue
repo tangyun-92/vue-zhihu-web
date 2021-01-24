6c9c2093c9a8089d20cd70d0e9b8337a023e4bb0
tangyun <tangyun1992@qq.com>
2021-01-24 14:52:53 +0800

    完成GlobalHeader组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="currentUser"></global-header>
     <column-list :list="list"></column-list>
   </div>
 </template>
@@ -8,12 +9,20 @@
 import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+
+const currentUser: UserProps = {
+  isLogin: true,
+  name: 'zs'
+}
+
 const testData: ColumnProps[] = [
   {
     id: 1,
     title: 'test1专栏',
     description: '这是test1专栏，有一段有意思的简介，可以更新一下',
-    avatar: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
+    avatar:
+      'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
   },
   {
     id: 2,
@@ -25,22 +34,25 @@ const testData: ColumnProps[] = [
     id: 3,
     title: 'test3专栏',
     description: '这是test2专栏，有一段有意思的简介，可以更新一下',
-    avatar: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
+    avatar:
+      'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
   },
   {
     id: 4,
     title: 'test4专栏',
     description: '这是test2专栏，有一段有意思的简介，可以更新一下',
-    avatar: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
+    avatar:
+      'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
   }
 ]
 
 export default defineComponent({
   name: 'App',
-  components: { ColumnList },
+  components: { ColumnList, GlobalHeader },
   setup() {
     return {
-      list: testData
+      list: testData,
+      currentUser
     }
   }
 })
