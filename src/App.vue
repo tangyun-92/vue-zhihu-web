9375b61891cafe0c2435d503c801cb090b8f41c5
tangyun <tangyun1992@qq.com>
2021-01-25 21:47:57 +0800

    完成组件中v-mode双向数据绑定

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,85 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <column-list :list="list"></column-list>
+    <!-- <column-list :list="list"></column-list> -->
+    <form action="">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <validate-input :rules="emailRules" v-model="emailVal"></validate-input>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+          aria-describedby="emailHelp"
+        />
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+// import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
 
 const currentUser: UserProps = {
   isLogin: true,
   name: 'zs'
 }
 
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1专栏',
-    description: '这是test1专栏，有一段有意思的简介，可以更新一下',
-    avatar:
-      'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
-  },
-  {
-    id: 2,
-    title: 'test2专栏',
-    description: '这是test2专栏，有一段有意思的简介，可以更新一下'
-    // avatar: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
-  },
-  {
-    id: 3,
-    title: 'test3专栏',
-    description: '这是test2专栏，有一段有意思的简介，可以更新一下',
-    avatar:
-      'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
-  },
-  {
-    id: 4,
-    title: 'test4专栏',
-    description: '这是test2专栏，有一段有意思的简介，可以更新一下',
-    avatar:
-      'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
-  }
-]
+// const testData: ColumnProps[] = [
+//   {
+//     id: 1,
+//     title: 'test1专栏',
+//     description: '这是test1专栏，有一段有意思的简介，可以更新一下',
+//     avatar:
+//       'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
+//   },
+//   {
+//     id: 2,
+//     title: 'test2专栏',
+//     description: '这是test2专栏，有一段有意思的简介，可以更新一下'
+//     // avatar: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
+//   },
+//   {
+//     id: 3,
+//     title: 'test3专栏',
+//     description: '这是test2专栏，有一段有意思的简介，可以更新一下',
+//     avatar:
+//       'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
+//   },
+//   {
+//     id: 4,
+//     title: 'test4专栏',
+//     description: '这是test2专栏，有一段有意思的简介，可以更新一下',
+//     avatar:
+//       'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'
+//   }
+// ]
 
 export default defineComponent({
   name: 'App',
-  components: { ColumnList, GlobalHeader },
+  components: {
+    // ColumnList,
+    GlobalHeader,
+    ValidateInput
+  },
   setup() {
+    const emailVal = ref('tangy')
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱地址' }
+    ]
     return {
-      list: testData,
-      currentUser
+      // list: testData,
+      currentUser,
+      emailRules,
+      emailVal
     }
   }
 })
---
+++ src/components/ValidateInput.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="validate-input-container pb-3">
+    <input
+      type="text"
+      class="form-control"
+      :class="{ 'is-invalid': inputRef.error }"
+      :value="inputRef.val"
+      @input="updateValue"
+      @blur="validateInput"
+    />
+    <span v-if="inputRef.error" class="invalid-feedback">{{
+      inputRef.message
+    }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, PropType } from 'vue'
+const emailReg = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
+const phoneReg = /^1[3456789]\d{9}$/
+
+interface RuleProp {
+  type: 'required' | 'email' | 'phone'
+  message: string
+}
+export type RulesProp = RuleProp[]
+
+export default defineComponent({
+  props: {
+    rules: Array as PropType<RulesProp>,
+    modelValue: String
+  },
+  setup(props, context) {
+    const inputRef = reactive({
+      val: props.modelValue || '',
+      error: false,
+      message: ''
+    })
+    const updateValue = (e: KeyboardEvent) => {
+      const targetValue = (e.target as HTMLInputElement).value
+      inputRef.val = targetValue
+      context.emit('update:modelValue', targetValue)
+    }
+    const validateInput = () => {
+      if (props.rules) {
+        const allPassed = props.rules.every((rule) => {
+          let passed = true
+          inputRef.message = rule.message
+          switch (rule.type) {
+            case 'required':
+              passed = inputRef.val.trim() !== ''
+              break
+            case 'email':
+              passed = emailReg.test(inputRef.val)
+              break
+            case 'phone':
+              passed = phoneReg.test(inputRef.val)
+              break
+            default:
+              break
+          }
+          return passed
+        })
+        inputRef.error = !allPassed
+      }
+    }
+    return {
+      inputRef,
+      validateInput,
+      updateValue
+    }
+  }
+})
+</script>
